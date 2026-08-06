b98d0a351bfa5d72e94e677096e494727fa7ea44
ppjh <17399968+rainpjh@user.noreply.gitee.com>
2026-08-06 20:33:16 +0800

    版本8_06

---
+++ shijue02/zixi8_06/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace zixi8_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 1;
+            //int j = 0;
+            //while (i <= 10)
+            //{
+            //    j = j + i;
+            //    i++;
+            //}
+            //Console.WriteLine($"总和：{j},i => {i}");
+
+            /**********************************************/
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("嘻嘻");
+            //}
+
+            /**********************************************/
+
+            //int i = 1;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //} while (i <= 5);
+
+            /**********************************************/
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("我和你");
+            //}
+
+            /**********************************************/
+
+            //for (int i = 10; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            /**********************************************/
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    if(i%2 == 0) Console.WriteLine(i);
+            //}
+
+            /**********************************************/
+
+            //double money = 10;
+            //int year = 1;
+            //while (year <= 50)
+            //{
+            //    money = money + money * 0.05;
+            //    year++;
+            //}
+            //Console.WriteLine($"50年后的月薪{money:F2}k");
+
+            /**********************************************/
+
+            //List<string> sList = new()
+            //{
+            //    "aa",
+            //    "bb",
+            //    "cc",
+            //    "dd",
+            //    "ee"
+            //};
+            //for (int i = 0; i < sList.Count; i++)
+            //{
+            //    Console.WriteLine(sList[i]);
+            //}
+
+            /**********************************************/
+
+            //List<int> intlist = new()
+            //{ 
+            //    1,
+            //    2,
+            //    3,
+            //    4,
+            //    5
+            //};
+            //int sum = 0;
+            //for (int i = 0; i < intlist.Count; i++)
+            //{
+            //    sum += intlist[i];
+            //}
+            //Console.WriteLine(sum);
+        }
+    }
+}
--- shijue02/zuoye8_05/obj/Debug/net8.0/zuoye8_05.AssemblyInfo.cs
+++ shijue02/zuoye8_05/obj/Debug/net8.0/zuoye8_05.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zuoye8_05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7305ab2dd691fcb62ae29bbfbe4a168b517c2939")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5362b6d72762ce707431c1ea06edfe290b7feec")]
 [assembly: System.Reflection.AssemblyProductAttribute("zuoye8_05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zuoye8_05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
