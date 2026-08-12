c19b1145e42fe76440944ee39fd325252e5cd7dc
ppjh <17399968+rainpjh@user.noreply.gitee.com>
2026-08-12 20:26:01 +0800

    版本8_12

---
+++ shijue02/zixi8_12/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace zixi8_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //用函数封装一个猜数字的小游戏，函数中生成一个随机整数（0 - 100）作为目标数字，
+            //不停的让用户输入数字，距离目标数字偏大，就提示用户偏大，
+            //距离目标数字偏小就输出偏小，用户有5次输入的机会，5次没有猜对，
+            //输出GAME OVER，猜对了就输出WIN！
+            //void nums() 
+            //{
+            //    Random r = new Random();
+            //    int numRandom = r.Next(100); // 获取0-100的随机整数，不包含100
+            //    int count = 0;
+            //    for (int i = 0; i <= 5; i++)
+            //    {
+            //        Console.WriteLine("请输入数字（整数0~100）：");
+            //        int numble = int.Parse(Console.ReadLine());
+            //        if (numble > 100 || numble < 0)
+            //        {
+            //            Console.WriteLine("！！！输入数字不在范围内");
+            //            break;
+            //        }
+            //        if (numble == numRandom)
+            //        {
+            //            Console.WriteLine("WIN");
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            if (numble > numRandom) { Console.WriteLine("偏大"); }
+            //            else { Console.WriteLine("偏小"); }
+            //            count++;
+            //        }
+            //        if (count == 5) { Console.WriteLine("GAME OVER"); break; }
+            //    }
+            //}
+            //nums();
+
+            /***********************************************************************/
+
+            //装修房间：参数1，圆的半径，计算圆的面积，每平方米收费200元，
+            //返回装修总价。计算这个半径的圆装修一半需要多少钱？
+
+            var zhuangxiu = (double r) =>
+            {
+                double money = Math.PI*r*r* 200;
+                return money;
+            };
+            double res = zhuangxiu(5);
+            Console.WriteLine($"{res:F2}");
+            Console.WriteLine($"装修一半需要的钱：{res/2:F2}");
+
+            
+
+
+
+
+        }
+    }
+}
---
+++ shijue02/zuoye8_12/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace zuoye8_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //作业一
+            //装修房间：参数1，圆的半径，计算圆的面积，每平方米收费200元，
+            //返回装修总价。计算这个半径的圆装修一半需要多少钱？
+            //var zhuangxiu = (double r) =>
+            //{
+            //    double money = Math.PI * r * r * 200;
+            //    return money;
+            //};
+            //double res = zhuangxiu(5);
+            //Console.WriteLine($"{res:f2}");
+            //Console.WriteLine($"装修一半的价格是{res / 2:f2}");
+
+            //作业二
+            //计算字符在字符串中出现的次数：参数1字符串，参数2某个字符，函数统计次数，并返回
+            //var strings = (string n, char m) => 
+            //{ 
+            //    if (!n.Contains(m)) return 0; 
+            //    char[] str =  n.ToArray();
+            //    int count = 1;
+            //    for(int i = 0; i < n.Length;i++)
+            //    {
+            //        if (str[i] == m)
+            //        {
+            //            for (int j = i + 1; j < n.Length; j++)
+            //            {
+            //                if (str[i] == str[j]) count++;
+            //            }
+            //            break;
+            //        }
+            //    }                
+            //    return count;
+            //};
+            //int res = strings("bababaddaab", 'a');
+            //Console.WriteLine(res);
+
+            //作业三
+            //计算一个整型数组中，最小值第一次出现的下标。
+            //方法一
+            //int[] arr = [10, 20, 5, 5, 50, 1, 7];
+            //Func<int[] ,int> getFirstMinIndex = (int[] nums) =>
+            //{
+            //    if (nums == null || nums.Length == 0)
+            //    {
+            //        return -1;
+            //    }
+            //    int min = nums[0];
+            //    for (int i = 1; i < nums.Length; i++)
+            //    {
+            //        if (nums[i] < min)
+            //        {
+            //            min = nums[i];
+            //        }
+            //    }
+            //    for (int j = 0; j < nums.Length; j++)
+            //    {
+            //        if (nums[j] == min)
+            //        { 
+            //            int index = j;
+            //            return index;
+            //        }
+            //    }
+            //    return -1;
+            //};
+            //int res = getFirstMinIndex(arr);
+            //Console.WriteLine(res);
+
+            //方法二
+            //int[] arr = [10, 20, 5, 5, 50, 6, 7];
+            //Func<int[], int> getFirstMinindex = (int[] nums) =>
+            //{
+            //    if(nums == null || nums.Length == 0) return 0;
+            //    int minIndex = 0;
+            //    for (int i = 1; i < nums.Length; i++)
+            //    {
+            //        if (nums[i] < nums[minIndex])
+            //        { 
+            //            minIndex = i;
+            //        }
+            //    }return minIndex;
+            //};
+            //int res = getFirstMinindex(arr);
+            //Console.WriteLine(res);
+
+
+            //作业四
+            //判断一个字符串是否为回文，返回布尔值类型。
+            string str = "bbcbbcbb";//aaa接收str
+            var strings = (string aaa) =>
+            {
+                char[] bbb = aaa.ToArray();
+                //if (bbb == bbb.Reverse().ToArray())
+                //{
+                //    return true;
+                //}return false;
+                int count = 0;
+                for (int i = 0; i < bbb.Length; i++)
+                {
+                    if (bbb[i] == bbb.Reverse().ToArray()[i])
+                    {
+                        count++;
+                    }
+                    if (count == bbb.Length)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
+            bool res = strings(str);
+            Console.WriteLine(res);
+
+
+
+
+        }
+    }
+}
